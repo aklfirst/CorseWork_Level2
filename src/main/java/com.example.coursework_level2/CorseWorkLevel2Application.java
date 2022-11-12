d2c7edcc1ddf250af056745edd3c7a9a2ec1580a
Alexander Klepov <a_klepov@mail.ru>
2022-11-12 21:23:47 +0300

    Initial commit

---
+++ src/main/java/com.example.coursework_level2/CorseWorkLevel2Application.java
@@ -0,0 +1,13 @@
+package com.example.corsework_level2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorseWorkLevel2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorseWorkLevel2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/corsework_level2/CorseWorkLevel2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corsework_level2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorseWorkLevel2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
